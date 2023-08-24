c78a625ac03517d469404a91ec768a019b7cbabc
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-08-25 00:17:32 +0600

    Basic layout with daisyui

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <button className='btn'>Button</button>
+      <button className='btn btn-neutral'>Neutral</button>
+      <button className='btn btn-primary'>Primary</button>
+      <button className='btn btn-secondary'>Secondary</button>
+    </>
+  );
+}
+
+export default App;
