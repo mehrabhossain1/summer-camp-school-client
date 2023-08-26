200ff4985cbfd2dfb419bdd3ad3ebfde65e3401b
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-08-26 16:25:18 +0600

    AuthProvider creating

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,13 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 
 const Main = () => {
+  const location = useLocation();
+  const hoHeaderFooter = location.pathname.includes("login");
+
   return (
     <div>
-      <Navbar />
+      {hoHeaderFooter || <Navbar />}
       <Outlet />
-      <Footer />
+      {hoHeaderFooter || <Footer />}
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Routes";
 import Container from "./components/Container/Container";
+import AuthProvider from "./providers/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Container>
-      <RouterProvider router={router} />
-    </Container>
+    <AuthProvider>
+      <Container>
+        <RouterProvider router={router} />
+      </Container>
+    </AuthProvider>
   </React.StrictMode>
 );
